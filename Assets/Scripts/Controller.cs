44012362b85e0b474281227f7c9f65a825cf9b58
Tomasz Trzaska <tommy.trzaska@gmail.com>
2015-08-22 09:02:26 +0200

    Moving player.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public int hp;
+	public float speed;
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Controller {
+
+
+	void Update () 
+	{
+		float vertical = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		float horizontal = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		Vector3 direction = new Vector3 (horizontal, vertical, 0);
+
+		GetComponent<Rigidbody2D> ().MovePosition (transform.position + direction);
+	}
+
+
+}
